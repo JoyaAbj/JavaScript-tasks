fcd99b8d8173bb755218238c4d6d4454e35ea5cb
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-07-25 15:43:44 +0300

    task_3

---
+++ task_3/script.js
@@ -0,0 +1,15 @@
+const num1="7";
+var num="4";
+let canPass=true;
+if (canPass==false){
+if (num1=>num) {
+    console.log ("Hello");
+} else  {
+    console.log ("Bye");
+}
+} else {
+    console.log("Try again");
+}
+
+
+
