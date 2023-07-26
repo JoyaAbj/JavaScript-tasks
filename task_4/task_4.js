3923de938cb7c9efe170219f1246e9cdea04b9be
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-07-26 10:19:08 +0300

    done with task_4

--- task_4/task_4.js
+++ task_4/task_4.js
@@ -4,3 +4,5 @@ var arrayNames =["joya" ,"joya", "joya"]
 console.log (arrayNames)
 var arrayMix = [4, "Joya", 40, "Abj"]
 console.log(arrayMix)
+let task4 = ArrayNumber + " " + arrayNames + " " + arrayMix
+console.log (task4)
