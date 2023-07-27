177076552998b6940910a506209f04ea8bdf45ed
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-07-27 09:30:23 +0300

    done with task_5

---
+++ task_5/task_5.js
@@ -0,0 +1,8 @@
+let numbers = [1, 2, 3, 4, 20, 17, 52, 90, 13, 55]
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers [i] % 2 == 0) {
+        console.log ( numbers [i] + ' This number is even ' )
+    } else {
+        console.log ( numbers [i] + ' This number is odd ' )
+    }
+}
